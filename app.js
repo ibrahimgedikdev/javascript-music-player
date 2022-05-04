2dbcebb47d69ee36cd32106570038998bf39b016
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-05-04 15:21:39 +0300

    fix ui and bug

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 var allSong = [
   {
-    artist: "Pilli Bebek",
-    song: "Haram Geceler",
-    songSrc: "haramgeceler.mp3",
+    artist: "Luke Combs, Billy Strings ",
+    song: "The Great Divide",
+    songSrc: "data/the-great-divide.mp3",
+    songImg: "data/the-great-divide.jpeg",
   },
   {
-    artist: "Kahraman Deniz",
-    song: "Suç Mahalli",
-    songSrc: "sucmahalli.mp3",
+    artist: "Danny Vera",
+    song: "Roller Coaster",
+    songSrc: "data/roller-coaster.mp3",
+    songImg: "data/roller-coaster.jpeg",
   },
   {
-    artist: "Melike Şahin",
-    song: "Tutuşmuş Beraber",
-    songSrc: "tutusmusberaber.mp3",
+    artist: "Hollow Coves",
+    song: "Home",
+    songSrc: "data/home.mp3",
+    songImg: "data/home.jpeg",
+  },
+  {
+    artist: "Gavin James",
+    song: "City Of Stars",
+    songSrc: "data/city-of-stars.mp3",
+    songImg: "data/city-of-stars.jpeg",
   },
 ];
 
-let bodyBg = document.querySelector(".body-bg");
-let modal = document.querySelector(".modal");
+let musicBg = document.querySelector(".music-bg");
+let songImg = document.querySelector(".song-img");
 let randomPlayButton = document.querySelector("#random");
 let previousButton = document.querySelector("#prev");
 let nextButton = document.querySelector("#next");
 let autoButton = document.querySelector("#auto");
 let playButton = document.querySelector("#play");
-let directPlay = document.querySelector("#directPlay");
+let musicList = document.querySelector(".music-list");
+let libraries = document.querySelector(".libraries");
+let openLibraryBtn = document.querySelector("#openLibraryBtn");
+let closeLibraryBtn = document.querySelector("#closeLibraryBtn");
+
+let html;
 
 let durationRange = document.querySelector(".duration-range");
 let currentTime = document.querySelector(".time");
@@ -31,18 +45,30 @@ let currentTime = document.querySelector(".time");
 let audio = new Audio();
 let currentSong = 0;
 
+let isPlaying = false
+
 window.onload = playingSong;
 
-directPlay.addEventListener("click", function () {
-  modal.style.display = "none";
-  bodyBg.style.display = "none";
-  playButton.innerHTML = '<i class="fas fa-pause"></i>';
-  playingSong();
-});
+audio.onplaying = function () {
+  isPlaying = true;
+};
+audio.onpause = function () {
+  isPlaying = false;
+};
 
 function playingSong() {
   audio.src = allSong[currentSong].songSrc;
+  musicBg.src = allSong[currentSong].songImg;
+  songImg.src = allSong[currentSong].songImg;
+
+  $(".song-artist").html(allSong[currentSong].artist);
+  $(".song-name").html(allSong[currentSong].song);
   audio.play();
+  if(isPlaying == true){
+    playButton.innerHTML = '<i class="fas fa-pause"></i>'
+  } else {
+    playButton.innerHTML = '<i class="fas fa-play"></i>'
+  }
 }
 
 playButton.addEventListener("click", () => {
@@ -62,7 +88,7 @@ audio.addEventListener("timeupdate", function () {
   convertTime(Math.round(audio.currentTime));
 
   if (audio.ended) {
-    nexSong();
+    // nextSong();
   }
 });
 
@@ -88,11 +114,9 @@ function totalTime(seconds) {
 
 nextButton.addEventListener("click", () => {
   currentSong++;
-  if (currentSong > 2) {
+  if (currentSong > allSong.length - 1) {
     currentSong = 0;
   }
-
-  console.log(currentSong);
 
   playingSong();
   playButton.innerHTML = '<i class="fas fa-pause"></i>';
@@ -104,7 +128,7 @@ nextButton.addEventListener("click", () => {
 previousButton.addEventListener("click", () => {
   currentSong--;
   if (currentSong < 0) {
-    currentSong = 2;
+    currentSong = allSong.length - 1;
   }
 
   playingSong();
@@ -112,4 +136,45 @@ previousButton.addEventListener("click", () => {
 
   $(".song-artist").html(allSong[currentSong].artist);
   $(".song-name").html(allSong[currentSong].song);
+});
+
+allSong.forEach((music, index) => {
+  html = `
+    <li class="music-item" song-id="${index}">
+     <div class="music-item-left">
+      <img src="${music.songImg}" class="music-item-img" />
+     </div>
+     <div class="music-item-center">
+        <h5 class="music-item-name">${music.song}</h5>
+        <p class="music-item-artist">${music.artist}</p>
+     </div>
+     <div class="music-item-right">
+        <button id="libraryPlay"><i class="fas fa-play"></i></button>
+     </div>
+    </li>
+  `;
+  musicList.innerHTML += html;
+});
+
+let musicItem = document.querySelectorAll(".music-item");
+
+openLibraryBtn.addEventListener("click", function () {
+  libraries.classList.add("active");
+
+  musicItem.forEach((element, index) => {
+    element.addEventListener("click", function () {
+      audio.src = allSong[index].songSrc;
+      musicBg.src = allSong[index].songImg;
+      songImg.src = allSong[index].songImg;
+      $(".song-artist").html(allSong[index].artist);
+      $(".song-name").html(allSong[index].song);
+      currentSong = index;
+      audio.play();
+      playButton.innerHTML = '<i class="fas fa-pause"></i>';
+      libraries.classList.remove("active");
+    });
+  });
+});
+closeLibraryBtn.addEventListener("click", function () {
+  libraries.classList.remove("active");
 });
